5b777c03e97e41c06a5583b5d6fc97d69343a5ba
Chythanyaramdas <cchythanyaramdas@gmail.com>
2024-06-28 01:03:32 +0530

    cors error

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const app = express();
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname,'public')))
-app.use(cors());
+app.use(cors({ origin: 'https://trippy-app-i5ac.vercel.app/' }));
 
 
 
